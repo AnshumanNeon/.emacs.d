de604b286597b9e15ebda90ba00e2170b7eecbf0
AnshumanNeon <anshumanneon@gmail.com>
2024-02-21 20:44:46 +0530

    formatted god-mode.el

--- god-mode.el
+++ god-mode.el
@@ -1,3 +1,7 @@
+;;; package --- Summary
+;;; Commentary:
+;;; none
+;;; Code:
 (require 'god-mode)
 (god-mode)
 
@@ -6,11 +10,13 @@
 (setq god-exempt-predicates nil)
 
 (defun my-god-mode-update-cursor-type ()
+  "Update cursor when god-mode enabled/disabled."
   (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
 
 (add-hook 'post-command-hook #'my-god-mode-update-cursor-type)
 
 (defun my-god-mode-update-mode-line ()
+  "Update mode-line when god-mode enabled/disabled."
   (cond
    (god-local-mode
     (set-face-attribute 'mode-line nil
@@ -28,3 +34,4 @@
 			:background "#2a2a2c"))))
 
 (add-hook 'post-command-hook #'my-god-mode-update-mode-line)
+;;; god-mode.el ends here
