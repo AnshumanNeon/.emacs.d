3ea22c989c6940bd080dee7fd332edfd77b72de7
AnshumanNeon <anshumanneon@gmail.com>
2024-03-02 18:48:21 +0530

    Revert "pdf-tools now work"
    This reverts commit d07c12d1fef7ed05b654716ba172ec1ec51b0ad7.

--- init.el
+++ init.el
@@ -18,14 +18,6 @@
 ;; terminal bind key
 (bind-key* "C-c C-;" #'execute-extended-command)
 
-;; pdf-tools
-(use-package pdf-tools
-  :ensure t
-  :config
-  (pdf-tools-install t))
-
-(pdf-loader-install t)
-
 ;; gcmh
 (load-file "~/.emacs.d/gcmh.el")
 (gcmh-mode 1)
@@ -207,16 +199,7 @@
 (use-package pdf-tools
   :ensure t
   :config
-  (pdf-tools-install t))
-
-(pdf-loader-install t)
-
-;; epub (ebook file format) reader
-;; nov.el
-(use-package nov
-  :ensure t
-  :config
-  (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode)))
+  (pdf-tools-install))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
