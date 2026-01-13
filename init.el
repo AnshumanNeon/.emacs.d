a18f1d9ca27cad0cc5070a7943dd6b415f46477f
AnshumanNeon <anshumanneon@gmail.com>
2026-01-13 13:23:02 +0530

    golden-ratio

--- init.el
+++ init.el
@@ -54,6 +54,11 @@
 ;; install magit
 (require 'magit)
 
+;; golden-ratio
+;; install golden-ratio
+(require 'golden-ratio)
+(golden-ratio-mode t)
+
 ;; ----------------------------------------------------------
 ;; ----------------------------------------------------------
 (require 'package)
@@ -68,7 +73,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(## company magit punch-line telephone-line transient with-editor)))
+   '(## company golden-ratio magit punch-line telephone-line transient
+	with-editor)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
